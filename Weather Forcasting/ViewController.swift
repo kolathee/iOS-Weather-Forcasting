fecdb6e26411acbab77a43fe44e7e389553eb98a
Kolathee Payuhawattana <kolathee@icloud.com>
2020-06-22 16:15:51 +0700

    Initial Commit

---
+++ Weather Forcasting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Forcasting
+//
+//  Created by Kolathee Payuhawattana on 22/6/2563 BE.
+//  Copyright © 2563 Cup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Weather ForcastingTests/Weather_ForcastingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Weather_ForcastingTests.swift
+//  Weather ForcastingTests
+//
+//  Created by Kolathee Payuhawattana on 22/6/2563 BE.
+//  Copyright © 2563 Cup. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_Forcasting
+
+class Weather_ForcastingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
